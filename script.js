bc1d17c19230d1b58a00ac7abe0235f478c0cfc8
Maxime Stojanovic <maxime.stojanovic@gmail.com>
2017-03-15 16:45:51 +0100

    v1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+function Rechercher()
+{
+	var ldlc="https://www.ldlc.com/navigation/";
+    var topachat = "https://www.topachat.com/pages/recherche.php?";
+	var materiel = "https://www.materiel.net/achat/";
+	var google = "www.google.fr"
+
+    var ref = document.getElementById("ref");
+    var refUrl = ref.replace(" ", "+");
+    document.write("refUrl");
+    
+
+
+}

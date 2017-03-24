5683f63386578838774521e133a46cdd19005cb0
Maxime Stojanovic <maxime.stojanovic@gmail.com>
2017-03-24 16:51:10 +0100

    modif mineures

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 var http = require("http");
+var fs = require("fs");
 
 http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write('Accueil');
-    if (page == '/') {
-        response.redirect('index.html');
-    }
-    response.end();
+    fs.readFile("site_web/index.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        console.log("server loaded");
+        response.write(data);
+        response.end();
+    });
 }).listen(8888);
-
-/*function Rechercher()
-{
-	var ldlc="https://www.ldlc.com/navigation/";
-    var topachat = "https://www.topachat.com/pages/recherche.php?";
-	var materiel = "https://www.materiel.net/achat/";
-	var google = "www.google.fr"
-
-    var ref = document.getElementById("ref");
-    var refUrl = ref.replace(" ", "+");
-    console.log(refUrl);
-    
-
-
-}*/
---
+++ site_web/index.js
@@ -0,0 +1,13 @@
+function Rechercher() {
+    var ldlc = "https://www.ldlc.com/navigation/";
+    var topachat = "https://www.topachat.com/pages/recherche.php?";
+    var materiel = "https://www.materiel.net/achat/";
+    var google = "www.google.fr"
+
+    var ref = document.getElementById("ref");
+    var refUrl = ref.replace(" ", "+");
+    console.log(refUrl);
+
+
+
+}
